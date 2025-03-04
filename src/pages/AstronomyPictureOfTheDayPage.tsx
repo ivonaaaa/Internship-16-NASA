c24a07d2234e717dbc8325561483af9898065e4d
ivonaaaa <ivonaa.e@gmail.com>
2025-03-04 20:11:54 +0100

    Switch to custom hooks for APIs

--- src/pages/AstronomyPictureOfTheDayPage.tsx
+++ src/pages/AstronomyPictureOfTheDayPage.tsx
@@ -1,78 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { fetchAPOD } from "../services/APODService";
-import { APODData } from "../types/APODData";
+import React, { useState } from "react";
+import useAPOD from "../hooks/useAPOD";
 import APODCard from "../components/AstronomyPictureOfTheDayPage/APODCard";
 import { CircularProgress, Button } from "@mui/material";
 import { getLast20Days } from "../utils/getLast20Days";
 import "../styles/APOD-page.css";
 
 const APOD: React.FC = () => {
-  const [filteredImages, setFilteredImages] = useState<APODData[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const { start: initialStart, end: initialEnd } = getLast20Days();
+  const [startDate, setStartDate] = useState(initialStart);
+  const [endDate, setEndDate] = useState(initialEnd);
 
-  useEffect(() => {
-    const { start, end } = getLast20Days();
-    setStartDate(start);
-    setEndDate(end);
-    fetchImages(start, end);
-  }, []);
+  const {
+    data: filteredImages,
+    loading,
+    fetchNewData,
+  } = useAPOD(startDate, endDate);
 
-  const fetchImages = async (start: string, end: string) => {
-    setLoading(true);
-    try {
-      const data = await fetchAPOD(start, end);
-      const sortedData = data.sort((a, b) => b.date.localeCompare(a.date));
-      setFilteredImages(sortedData);
-    } catch (error) {
-      console.error("Failed to fetch APOD images:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleFilter = async () => {
+  const handleFilter = () => {
     if (!startDate || !endDate) return;
-
-    setLoading(true);
-    try {
-      const data = await fetchAPOD(startDate, endDate);
-      const sortedData = data.sort((a, b) => b.date.localeCompare(a.date));
-      setFilteredImages(sortedData);
-      setFilteredImages(sortedData);
-    } catch (error) {
-      console.error("Failed to fetch filtered APOD images:", error);
-    } finally {
-      setLoading(false);
-    }
+    fetchNewData(startDate, endDate);
   };
 
-  const loadMore = async () => {
+  const loadMore = () => {
     if (loading) return;
-    setLoading(true);
-    try {
-      const newEndDate = new Date(startDate);
-      newEndDate.setDate(newEndDate.getDate() - 1);
 
-      const newStartDate = new Date(newEndDate);
-      newStartDate.setDate(newStartDate.getDate() - 9);
+    const newEndDate = new Date(startDate);
+    newEndDate.setDate(newEndDate.getDate() - 1);
 
-      const moreData = await fetchAPOD(
-        newStartDate.toISOString().split("T")[0],
-        newEndDate.toISOString().split("T")[0]
-      );
-      setStartDate(newStartDate.toISOString().split("T")[0]);
+    const newStartDate = new Date(newEndDate);
+    newStartDate.setDate(newStartDate.getDate() - 9);
 
-      const sortedMoreData = moreData.sort((a, b) =>
-        b.date.localeCompare(a.date)
-      );
-      setFilteredImages((prev) => [...prev, ...sortedMoreData]);
-    } catch (error) {
-      console.error("Failed to load more APOD images:", error);
-    } finally {
-      setLoading(false);
-    }
+    setStartDate(newStartDate.toISOString().split("T")[0]);
+    fetchNewData(
+      newStartDate.toISOString().split("T")[0],
+      newEndDate.toISOString().split("T")[0],
+      true
+    );
   };
 
   return (
